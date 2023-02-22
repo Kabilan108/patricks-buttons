94203abc0072f6f49d54cb809fdbd51d788161a8
Tony Okeke <58089381+Kabilan108@users.noreply.github.com>
2023-02-22 01:26:32 -0500

    Use single LED

--- patricks-buttons/testing/blink.cpp
+++ patricks-buttons/testing/blink.cpp
@@ -1,20 +1,16 @@
 #include <Arduino.h>
 
-#define LED_1 13
-#define LED_2 12
+#define LED 13
 
 void setup() {
     // initialize digital pin LED_1 as an output.
-    pinMode(LED_1, OUTPUT);
-    pinMode(LED_2, OUTPUT);
+    pinMode(LED, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-    digitalWrite(LED_1, HIGH);  // turn the LED on (HIGH is the voltage level)
-    digitalWrite(LED_2, HIGH);
+    digitalWrite(LED, HIGH);  // turn the LED on (HIGH is the voltage level)
     delay(1000);                      // wait for a second
-    digitalWrite(LED_1, LOW);   // turn the LED off by making the voltage LOW
-    digitalWrite(LED_2, LOW);
+    digitalWrite(LED, LOW);   // turn the LED off by making the voltage LOW
     delay(1000);                      // wait for a second
 }

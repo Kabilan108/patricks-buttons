3d565f6d15c61398702cec0613673ea84a86b34c
Tony Okeke <58089381+Kabilan108@users.noreply.github.com>
2023-02-25 00:23:47 -0500

    Add options for different services

--- patricks-buttons/api/main.py
+++ patricks-buttons/api/main.py
@@ -2,10 +2,37 @@
 Main script for Patrick's Buttons API
 """
 
+from InquirerPy.base.control import Choice
+from InquirerPy import prompt
 from decouple import config
 import uvicorn
 
 
+def select_service():
+    """Select service to interact with"""
+
+    questions = [
+        {
+            "type": "list",
+            "message": "Select service to control",
+            "name": "service",
+            "choices": [
+                "Spotify",
+                "YouTube",
+                "Netflix",
+                Choice(name="Quit", value="quit")
+            ]
+        }
+    ]
+    answers = prompt(questions)
+    return answers["service"]
+
+
 if __name__ == "__main__":
     HOST, PORT = config("HOST"), int(config("PORT"))
-    uvicorn.run("api:app", host=HOST, port=PORT, reload=True)
+    service = select_service()
+    if service == "quit":
+        print("Goodbye!")
+    else:
+        app = f"api:{service.lower()}"
+        uvicorn.run(app, host=HOST, port=PORT, reload=True)
